0a4ea0da662a225698da71de4e7fd25501bc3425
unybhgfd <unybhgfd@outlook.com>
2025-11-06 13:41:29 +0800

    新消息通知

--- pre_releases/xes_code/d.js
+++ pre_releases/xes_code/d.js
@@ -2,6 +2,7 @@
 // 同时内容也会在tampermonkey脚本中
 
 
+// 修改菜单
 setInterval(async function () {
     try {
         let elemA = document.getElementsByClassName("user_icon_dropdown")[0].childNodes[2].childNodes[0];
@@ -15,3 +16,55 @@ setInterval(async function () {
         }
     } catch (e) {}
 }, 500);
+
+// 未读消息提醒
+await new Promise(resolve => {
+    setTimeout(resolve, 1000)
+})
+window.XESChat_msgListenerTitlePrefix = "";
+window.XESChat_msgListenerTitleName = document.title | "学而思编程-学而思网校-受益一生的能力";
+window.XESChat_msgListenerOriginalTitleSetter = Object.getOwnPropertyDescriptor(Document.prototype, "title").set;
+
+Object.defineProperty(document, "title", {
+    set: function(val) {
+        window.XESChat_msgListenerTitleName = val.toString();
+        window.XESChat_msgListenerOriginalTitleSetter.call(
+            document,
+            window.XESChat_msgListenerTitlePrefix + window.XESChat_msgListenerTitleName
+        )
+    },
+    get: function() {
+        return window.XESChat_msgListenerTitleName;
+    },
+    configurable: true
+});
+
+window.XESChat_msgListenerIntervalId = setInterval(async function () {
+    try {
+        fetch("https://code.xueersi.com/api/messages/overview", {
+            "headers": {
+                "accept": "application/json, text/plain, */*",
+            },
+            "method": "GET",
+        }).then(async (resp) => {
+            return await resp.json();
+        }).then((data) => {
+            /**
+             * @type {{
+             *     category: number,
+             *     text: string,
+             *     count: number
+             * }[]}
+             */
+            let msgList = data.data;
+            window.XESChat_msgListenerTitlePrefix = ""
+            for (let i in [2, 0, 3, 4, 1]) {
+                if (msgList[i].count !== 0) {
+                    window.XESChat_msgListenerTitlePrefix = `[${msgList[i].count.toString()}条${msgList[i].text}消息]`;
+                    break;
+                }
+            }
+            document.title = window.XESChat_msgListenerTitleName;
+        });
+    } catch {}
+}, 10_000);
